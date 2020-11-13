e0faa6a994b7f1b19c247dd390863742ed2f34b3
Adrianasanduta <sanduta.adriana9@gmail.com>
2020-11-13 21:08:22 +0200

    Add lab 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // lab1
 import SingletonPblFactory from "./CreationalPatterns/SingletonPblFactory";
 import PblPrototype from './CreationalPatterns/PblPrototype';
+import DecoratedPbl from './StructuralPatterns/DecorantedPbl';
+import GroupsProxy from './StructuralPatterns/GroupsProxy';
+import AplicantFacade from "./StructuralPatterns/AplicantFacade";
 
 const groups = [];
 const pblFactory1 = new SingletonPblFactory();
@@ -8,7 +11,6 @@ const pblFactory2 = new SingletonPblFactory();
 console.log(`\n============ Lab1, Creational Patterns ==============\n`);
 
 console.log(`pblFactory1 === pblFactory2 ${pblFactory1 === pblFactory2}`);
-console.log(`noPblFactory1 === noPblFactory2 ${noPblFactory1 === noPblFactory2}`);
 
 groups.push(pblFactory1.createPbl('FAF-181'));
 groups.push(pblFactory1.createPbl('FAF-182'));
@@ -19,7 +21,7 @@ groups.push(pblFactory1.createNoPbl('TI-162'));
 groups.push(pblFactory2.createNoPbl('TI-163'));
 groups.push(pblFactory2.createNoPbl('TI-164'));
 
-const proto = pblFactory1.create('FAF-183 Prototype');
+const proto = pblFactory1.createPbl('FAF-183 Prototype');
 const prototype = new PblPrototype(proto);
 
 groups.push(prototype.clone());
@@ -27,3 +29,23 @@ groups.push(prototype.clone());
 groups.forEach((group) => {
   group.say();
 });
+
+console.log(`\n============ Lab2, Structural Patterns ==============\n`);
+
+const group = pblFactory1.createPbl('FAF-201');
+group.say();
+const decoratedPblGroup = new DecoratedPbl(group, 'Adriana Sanduta');
+decoratedPblGroup.say();
+decoratedPblGroup.addStudent('Victoria Belinschi');
+decoratedPblGroup.addStudent('Sandrinia Saponari');
+decoratedPblGroup.removeStudent('Victoria Belinschi');
+decoratedPblGroup.addStudent('Ilona Sanduta');
+decoratedPblGroup.say();
+groups.push(decoratedPblGroup);
+
+console.log(`\nProxy\n`);
+const groupProxy = new GroupsProxy(groups);
+groupProxy.getFieldInfo('groupName');
+groupProxy.getFieldInfo('moderator');
+groupProxy.getFieldInfo('groupName');
+groupProxy.getCount();
